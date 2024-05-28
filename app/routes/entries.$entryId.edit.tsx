aa30c14499d6910b4565b5aa3fc7c483325d38c8
Pius Prince <45078705+piusprince@users.noreply.github.com>
2024-05-28 10:31:08 +0000

    feat: Add edit entry page

--- app/routes/entries.$entryId.edit.tsx
+++ app/routes/entries.$entryId.edit.tsx
@@ -4,12 +4,10 @@ import {
   ActionFunctionArgs,
   redirect,
 } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { Form, useLoaderData } from "@remix-run/react";
 import EntryForm from "~/components/entry-form";
 
 export async function loader({ params }: LoaderFunctionArgs) {
-  console.log({ params });
-
   if (typeof params.entryId !== "string") {
     throw new Response("Entry ID not found", { status: 404 });
   }
@@ -37,35 +35,42 @@ export async function action({ request, params }: ActionFunctionArgs) {
     throw new Response("Entry ID not found", { status: 404 });
   }
 
-  console.log({ params });
-
   const db = new PrismaClient();
 
   const formData = await request.formData();
-  const { date, category, text } = Object.fromEntries(formData);
+  const { _action, date, category, text } = Object.fromEntries(formData);
 
   await new Promise((resolve) => setTimeout(resolve, 1000));
 
-  if (
-    typeof date !== "string" ||
-    typeof category !== "string" ||
-    typeof text !== "string"
-  ) {
-    throw new Error("Invalid form data");
+  if (_action === "delete") {
+    await db.entry.delete({
+      where: {
+        id: +params.entryId,
+      },
+    });
+    return redirect("/");
+  } else {
+    if (
+      typeof date !== "string" ||
+      typeof category !== "string" ||
+      typeof text !== "string"
+    ) {
+      throw new Error("Invalid form data");
+    }
+
+    await db.entry.update({
+      where: {
+        id: +params.entryId,
+      },
+      data: {
+        type: category,
+        date: new Date(date),
+        text: text,
+      },
+    });
+
+    return redirect("/");
   }
-
-  await db.entry.update({
-    where: {
-      id: +params.entryId,
-    },
-    data: {
-      type: category,
-      date: new Date(date),
-      text: text,
-    },
-  });
-
-  return redirect("/");
 }
 
 export default function EditPage() {
@@ -73,11 +78,26 @@ export default function EditPage() {
 
   console.log({ entry });
 
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    if (!confirm("Are you sure you want to delete this entry?")) {
+      event.preventDefault();
+    }
+  };
+
   return (
-    <div>
+    <>
       Edit
       <p>Editing entry {entry.id}</p>
       <EntryForm entry={entry} />
-    </div>
+      <Form method="post" onSubmit={handleSubmit}>
+        <button
+          name="_action"
+          value="delete"
+          className="p-2 text-white bg-red-500"
+        >
+          Delete entry
+        </button>
+      </Form>
+    </>
   );
 }
